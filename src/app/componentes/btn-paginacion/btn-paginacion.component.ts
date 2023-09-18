c29690d59ae2d99f9ce9236296f741d417399e8f
Cristian <cristianbcm1999@gmail.com>
2023-09-18 19:53:10 -0300

    Fix: Hacer responsivos los botones de paginacion

--- src/app/componentes/btn-paginacion/btn-paginacion.component.ts
+++ src/app/componentes/btn-paginacion/btn-paginacion.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, OnInit, Output } from '@angular/core';
 import { Publicacion } from 'src/app/modelo/interfaces/publicacion';
 
 @Component({
@@ -21,9 +21,14 @@ export class BtnPaginacionComponent implements OnInit{
 
   ultimoElementoEnArray:number = 0;
 
-  constructor(){}
+  isMobile:boolean = window.innerWidth < 400;
+
+  constructor(){
+    
+  }
 
   ngOnInit(): void {
+    console.log();
     if(this.listaObjetos){
       this.cantidadPaginas = Math.ceil(this.listaObjetos.length / this.cantMostrarPublicaciones);
       console.log(this.cantidadPaginas + " cantidadPaginas")
@@ -38,9 +43,20 @@ export class BtnPaginacionComponent implements OnInit{
     }
     console.log(this.listaObjetos);
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any){
+    console.log("ESCUCHANDO HOSTLISTENER" + event.target.innerWidth);
+    const screenWidth = event.target.innerWidth;
+    console.log(screenWidth + " SCREENWIDTH");
+    this.isMobile = screenWidth < 470;
+  }
+
   get listaBotones():number[]{
+    let cantidadBotones = 0;
+    this.isMobile ? cantidadBotones = 1 : cantidadBotones = 4;
     const inicio = this.paginaActual - 1;
-    const fin = this.paginaActual + 3;
+    const fin = this.paginaActual + cantidadBotones;
     return this.arrayBotones.slice(inicio, fin);
   }
   cambiarPagina(nuevoNumero:number){
