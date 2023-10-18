b4c1bf956c820a3a43c7fca6c5b2a76d2678606b
CrisBCM <cristianbcm1999@gmail.com>
2023-10-18 11:28:44 -0300

    Feat: Añadir spinner en modal eliminar, registro y login

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 import { LoginServiceService } from 'src/app/service/login-service.service';
+import { SpinnerService } from 'src/app/service/spinner.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy{
 
   public msgError:any;
+  spinner:boolean = true;
+  onDestroy$:Subject<boolean> = new Subject();
 
-  constructor(private formBuilder:FormBuilder, private loginService:LoginServiceService, private router:Router){}
+  constructor(private formBuilder:FormBuilder, private loginService:LoginServiceService, private router:Router, private spinnerService:SpinnerService){}
+
+  ngOnInit(): void {
+    this.spinnerService.obtenerSpinner.pipe(takeUntil(this.onDestroy$)).subscribe((spinner:boolean)=>{
+      this.spinner = spinner;
+    })
+  }
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
+  }
 
   loginForm = this.formBuilder.group({
     'email': ["",[Validators.required]],
--- src/app/componentes/modal-eliminar/modal-eliminar.component.ts
+++ src/app/componentes/modal-eliminar/modal-eliminar.component.ts
@@ -1,18 +1,30 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
 import { ForoService } from 'src/app/service/foro.service';
+import { SpinnerService } from 'src/app/service/spinner.service';
 
 @Component({
   selector: 'app-modal-eliminar',
   templateUrl: './modal-eliminar.component.html',
   styleUrls: ['./modal-eliminar.component.css']
 })
-export class ModalEliminarComponent {
+export class ModalEliminarComponent implements OnInit, OnDestroy{
   @Input() tituloModal:String = "";
   @Input() mensajeModal:String = "";
   @Output() confirmarEliminar = new EventEmitter<void>;
   @Output() cerrarModal = new EventEmitter<boolean>;
-  constructor(private foroService:ForoService){
+  spinner:boolean = false;
+  onDestroy$:Subject<boolean> = new Subject();
+  constructor(private foroService:ForoService, private spinnerService:SpinnerService){
 
+  }
+  ngOnInit(): void {
+    this.spinnerService.obtenerSpinner.pipe(takeUntil(this.onDestroy$)).subscribe((spinner:boolean)=>{
+      this.spinner = spinner;
+    })
+  }
+  ngOnDestroy(): void {
+    this.onDestroy$.next(true);
   }
 
   confirmar(){
